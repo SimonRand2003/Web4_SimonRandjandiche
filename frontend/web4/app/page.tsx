533f4987d3d1a2a6495d0aa352bacf58dd43a078
Belal-Zourob <belal.zourob@student.ucll.be>
2023-04-24 12:09:25 +0200

    Next.js

--- frontend/web4/app/page.tsx
+++ frontend/web4/app/page.tsx
@@ -1,54 +1,72 @@
-import '../app/css/tailwind.css';
+'use client';
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface Movie {
-    _movieid: string;
+    _movieid: number;
     _title: string;
     _releaseDate: string;
     _duration: number;
 }
+
 async function getMovies() {
-        const response = await fetch('http://127.0.0.1:3000/movies');
-        const data = await response.json();
-        return data as Movie[];
+    const response = await fetch('http://127.0.0.1:3000/movies');
+    const data = await response.json();
+    return data as Movie[];
 }
+
 function MovieComponent({ movie }: { movie: Movie }) {
     const { _movieid, _title, _releaseDate, _duration } = movie;
 
     return (
         <tr>
-            <td className="border px-4 py-2">{_movieid}</td>
-            <td className="border px-4 py-2">{_title}</td>
-            <td className="border px-4 py-2">{_releaseDate}</td>
-            <td className="border px-4 py-2">{_duration}</td>
-            <td className="border px-4 py-2">add to list</td>
-            <td className="border px-4 py-2">Rate</td>
+            <td>{_movieid}</td>
+            <td>{_title}</td>
+            <td>{_releaseDate}</td>
+            <td>{_duration}</td>
+            <td>
+                <button className="btn btn-primary">Add to list</button>
+            </td>
+            <td>
+                <button className="btn btn-secondary">Rate</button>
+            </td>
         </tr>
     );
 }
 
-export default async function MoviesPage() {
-    const movies = await getMovies();
+export default function MoviesPage() {
+    const [movies, setMovies] = React.useState<Movie[]>([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getMovies();
+            setMovies(data);
+        }
+
+        fetchData();
+    }, []);
+
+
     return (
-        <div>
+        <div className="container">
             <h1>Movies</h1>
-            <table className="table-auto">
+            <table className="table table-striped">
                 <thead>
                 <tr>
-                    <th className="px-4 py-2">ID</th>
-                    <th className="px-4 py-2">Title</th>
-                    <th className="px-4 py-2">Release Date</th>
-                    <th className="px-4 py-2">Duration</th>
-                    <th className="px-4 py-2"></th>
-                    <th className="px-4 py-2"></th>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Release Date</th>
+                    <th>Duration</th>
+                    <th></th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
-                {movies?.map((movie) => {
+                {movies.map((movie) => {
                     return <MovieComponent key={movie._movieid} movie={movie} />;
                 })}
                 </tbody>
             </table>
         </div>
     );
-
 }
